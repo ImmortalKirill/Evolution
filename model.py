cd7a0f2c6ba481ba931604ca4c6679e1672a151c
Rail Khashaev <khashaev.rm@phystech.edu>
2021-11-12 18:07:34 +0300

    Remake fill to live

--- model.py
+++ model.py
@@ -10,30 +10,30 @@ def step(Field):
     pole = Field.cells.copy()
     for x in range(1, pole.size_x - 1, 1): #FixMe Now programm doesn't work with borders
         for y in range(1, pole.size_y - 1, 1):
-            if pole[x][y].fill:
+            if pole[x][y].live > 0:
                 neighbors = 0
-                if pole[x + 1][y].fill:
+                if pole[x + 1][y].live:
                     neighbors += 1
-                if pole[x - 1][y].fill:
+                if pole[x - 1][y].live:
                     neighbors += 1
-                if pole[x][y + 1].fill:
+                if pole[x][y + 1].live:
                     neighbors += 1
-                if pole[x][y - 1].fill:
+                if pole[x][y - 1].live:
                     neighbors += 1
                 if neighbors == 1 or neighbors == 4:
-                    pole[x][y].fill = False
+                    pole[x][y].live -= 1
             else:
                 neighbors = 0
-                if pole[x + 1][y].fill:
+                if pole[x + 1][y].live:
                     neighbors += 1
-                if pole[x - 1][y].fill:
+                if pole[x - 1][y].live:
                     neighbors += 1
-                if pole[x][y + 1].fill:
+                if pole[x][y + 1].live:
                     neighbors += 1
-                if pole[x][y - 1].fill:
+                if pole[x][y - 1].live:
                     neighbors += 1
                 if neighbors == 3:
-                    pole[x][y].fill = True
+                    pole[x][y].live += 1
     Field.cells = pole.copy()
             
                 
