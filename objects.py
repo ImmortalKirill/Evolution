2b5d8465fe6d7975b728613cf4ecba17a4a21c7d
kirill <leontev.kirill@phystech.edu>
2021-11-12 22:01:24 +0300

    added basic function names in objects and vis

---
+++ objects.py
@@ -0,0 +1,8 @@
+class Cell:
+    pass   # FixMe
+    """ class of one cell on a Field"""
+
+
+class Field:
+    """ class Field, consists of cells"""
+    pass # FixMe
---
+++ vis.py
@@ -0,0 +1,14 @@
+from objects import *
+def menu():
+    pass # FixMe
+    """ draws menu of the game with play button"""
+
+
+def interface():
+    pass # FixMe
+    """draws interface of the game with buttons and game-play window"""
+
+
+def game_field(field: Field):
+    pass # FixMe
+    """draws field on the screen in game-play window"""
