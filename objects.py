ed0c8036940717e7c491d0f4ff5652dfa0ec3a69
Rail Khashaev <khashaev.rm@phystech.edu>
2021-11-13 10:03:16 +0300

    Added classes

--- objects.py
+++ objects.py
@@ -1,8 +1,16 @@
-class Cell:
-    pass   # FixMe
+class Cell(x0, y0):
     """ class of one cell on a Field"""
+    def __init__():
+        live = self.live = 1
+        x = self.x = x0
+        y = self.y = y0
+        
 
 
-class Field:
+class Field(x, y):
     """ class Field, consists of cells"""
-    pass # FixMe
+    def __init__():
+        cells = [[Cell(l, i) for i in range(y)] for l in range(x)]
+        x_center = self.x_center = x / 2
+        y_center = self.y_center = y / 2
+        scale = self.scale = 1
