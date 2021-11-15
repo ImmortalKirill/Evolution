4c64ddad58087e30572c8ebe96a52d194568ff69
kirill <leontev.kirill@phystech.edu>
2021-11-15 10:56:35 +0300

    changed game window to interface

--- vis.py
+++ vis.py
@@ -11,9 +11,9 @@ def draw_interface():
     """draws interface of the game with buttons and game-play window"""
 
 
-def game_field(screen, field: Field, game_window):
+def game_field(screen, field: Field, interface_game_window):
     """draws field on the screen in game-play window"""
-    grid = find_grid(field, game_window)
+    grid = find_grid(field, interface_game_window)
     draw_grid(screen, grid)
     draw_life_cells(screen, field, grid)
 def draw_grid(screen, grid):
@@ -22,12 +22,12 @@ def draw_grid(screen, grid):
     for i in range(grid[2]+1):
         pygame.draw.line(screen, (255, 255, 255),
                          (grid[0] + i*grid[4], grid[1]),
-                         (grid[0] + i*grid[4], grid[1]+grid[4]*grid[3]), 2
+                         (grid[0] + i*grid[4], grid[1]+grid[4]*grid[3]), 1
                          )
     for i in range(grid[3]+1):
         pygame.draw.line(screen, (255, 255, 255),
                          (grid[0], grid[1] + i*grid[4]),
-                         (grid[0] + grid[4]*grid[2], grid[1] + i*grid[4]), 2
+                         (grid[0] + grid[4]*grid[2], grid[1] + i*grid[4]), 1
                          )
 def draw_life_cells(screen, field, grid):
     """draws life cells in field on grid"""
@@ -35,13 +35,13 @@ def draw_life_cells(screen, field, grid):
         for j in range(grid[3]):
             cell = field.cells[i][j]
             if cell.live:
-                pygame.draw.rect(screen, cell.color,
+                pygame.draw.ellipse(screen, cell.color,
                                  (grid[4]*cell.x + grid[0], grid[1]+grid[4]*grid[3] - (cell.y+1)*grid[4],
                                   grid[4], grid[4]), 0)
-def draw_game(screen, field, game_window):
+def draw_game(screen, field, interface):
     """draws game screen on par screen with field"""
-    game_field(screen, field, game_window)
-    draw_interface()
+    game_field(screen, field, interface.game_window)
+    interface.draw()
     
 if __name__ == "__main__":
     print("This module is not for direct call!")
