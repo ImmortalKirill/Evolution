517d62094aa1a1ff928f6cde5f5615f8a9ab4fb1
Rail Khashaev <khashaev.rm@phystech.edu>
2021-11-15 11:12:19 +0300

    Fixed model. Finally

--- objects.py
+++ objects.py
@@ -2,10 +2,11 @@ from random import randint
 class Cell():
     """ class of one cell on a Field"""
     def __init__(self):
-        live = self.live = 1
+        live = self.live = 0
         x = self.x = 0
         y = self.y = 0
         self.color = (255, 255, 255)
+        max_live = self.max_live = 1
         
     def new_cell(self, x0, y0):
         x = self.x = x0
@@ -31,12 +32,20 @@ class Field():
             for l in range(y):
                 self.cells[i][l] = Cell()
                 self.cells[i][l].new_cell(i, l)
-                if randint(0, 2):
-                    self.cells[i][l].live = 0
+                if randint(0, 15):
+                    self.cells[i][l].live = 1
+                #self.cells[50][50].live = 1
+                #self.cells[50][49].live = 1
+                #self.cells[50][48].live = 1
+        self.cells[5][5].live = 1
+        self.cells[5][4].live = 1
+        self.cells[5][3].live = 1        
         self.x_center = x / 2
         self.y_center = y / 2
         self.size_x = x
         self.size_y = y
+        
+        
     def change_cors(self, vector):
         """shift of center coordinates on vector(x, y)"""
         self.x_center += vector[0]
